f0796fa5162bc506aedfe9e807382bb61f85b570
Imchidex <144730149+Imchidex@users.noreply.github.com>
2024-10-17 11:48:05 -0400

    Added the Extern "C" for the functions into the unit test and made sure everything works by running the unit tests.

--- BCSRec_UnitTest/BCSRec_UnitTest.cpp
+++ BCSRec_UnitTest/BCSRec_UnitTest.cpp
@@ -5,6 +5,16 @@ extern "C"
 #include "../BCSRec/main.h"
 }
 
+extern "C"
+{
+	void setLength(int input, int* length);
+	void setWidth(int input, int* width);
+	int getPerimeter(int* length, int* width);
+	int getArea(int* length, int* width);
+	void printWelcomeMenu();
+	void printOptions();
+	int getIntInput(char message[]);
+}
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
